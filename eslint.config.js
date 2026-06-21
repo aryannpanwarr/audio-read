95e810313144fb0b3d0408316e67d6eda677053e
Aryan Panwar <aryannpanwarr@gmail.com>
2026-06-21 11:50:24 +0530

    Remove desktop audiobook experiment

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'audiobook-output', 'native']),
+  globalIgnores(['dist', 'native']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
